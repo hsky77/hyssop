d03a1662264d39cc82e2ec1646b6da7b44735a94
Howard Kung <howardlkung@outlook.com>
2020-09-05 23:28:31 +0800

    Initialize hyssop extension

---
+++ hyssop_extension/README.md
@@ -0,0 +1,16 @@
+# hyssop-extension
+
+[![Documentation Status](https://readthedocs.org/projects/hyssop/badge/?version=latest)](https://hyssop.readthedocs.io/en/latest/?badge=latest) [![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/MIT) [![PyPI version](https://img.shields.io/pypi/v/hyssop-extension.svg)](https://pypi.org/project/hyssop-extension/)
+
+**hyssop-extension** extends **hyssop** components and utilities.
+
+**prerequest**: python 3.6+, pip
+
+**Install** hyssop-extension with pip: ``pip install hyssop_extension``
+
+Read [documentation](https://hyssop.readthedocs.io/en/latest/) for more information
+
+## Change log
+
+* **1.0.0 - Sep. 02, 2020**:
+   * Initalize project.
--- setup_extension.py
+++ setup_extension.py
@@ -7,17 +7,19 @@
 File created: August 21st 2020
 
 Modified By: hsky77
-Last Updated: September 5th 2020 23:18:04 pm
+Last Updated: September 5th 2020 23:17:55 pm
 '''
 
 import shutil
 import setuptools
-from hyssop import Version, __name__
+
+from hyssop import Version as hy_ver
+from hyssop_extension import Version, __name__
 
 shutil.rmtree('dist')
 shutil.rmtree('build')
 
-with open("README.md", "r") as fh:
+with open("hyssop_extension/README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
@@ -25,12 +27,12 @@ setuptools.setup(
     version=Version,
     author="hsky77",
     author_email="howardlkung@gmail.com",
-    description="component-based architecture and project hierarchy based on opensource web framework",
+    description="Extended hyssop components and utilities",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/hsky77/hyssop",
     packages=setuptools.find_packages(
-        exclude=('hyssop_extension', 'hyssop_extension.*')),
+        include=('hyssop_extension.*',), exclude=('hyssop', 'hyssop.*')),
     license="MIT License",
     classifiers=[
         "Programming Language :: Python :: 3",
@@ -39,12 +41,8 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=['tornado>=6.0.3',
-                      'tornado-swagger>=1.2.4',
-                      'PyYAML>=5.1.1',
-                      'coloredlogs>=10.0',
-                      'aiohttp>=3.6.2',
-                      'requests>=2.20.0'],
+    install_requires=['hyssop>=' + hy_ver,
+                      'sqlalchemy>=1.3.5'],
     python_requires='>=3.6',
     package_data={'': ['*.yaml', '*.csv']}
 )
